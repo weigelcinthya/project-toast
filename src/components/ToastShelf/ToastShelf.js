5a6c76d995d680a5563cf625a41a6fc5917e63b0
weigelcinthya <weigelcinthya@gmail.com>
2025-12-29 10:05:19 -0300

    adding provider component and using context instead of passing props]

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -2,13 +2,17 @@ import React, { useEffect } from 'react';
 
 import Toast from '../Toast';
 import styles from './ToastShelf.module.css';
+import { ToastContext } from '../ToastProvider/ToastProvider';
 
-function ToastShelf({messages, handleClose}) {
+function ToastShelf() {
+
+  const { messages } = React.use(ToastContext)
+
   return (
     <ol className={styles.wrapper}>
       {messages.length > 0 && messages.map(message => (
         <li className={styles.toastWrapper} key={message.id}>
-          <Toast toast={message} handleClose={handleClose}/>
+          <Toast toast={message}/>
         </li>
       ))}
     </ol>
